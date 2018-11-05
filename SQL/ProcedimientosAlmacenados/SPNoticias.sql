34bd07729a53fdd8963e0f5590557c6d378457e5
VictorMunayco96 <m.m.victor9630@gmail.com>
2018-11-05 14:58:15 -0500

    ProcedimientosAlmacenados

---
+++ SQL/ProcedimientosAlmacenados/SPNoticias.sql
@@ -0,0 +1,47 @@
+Use pagadmin	;
+
+DELIMITER $$
+ CREATE PROCEDURE PA_Set_Noticias(
+in _IdNoticias  int(11),
+in _TituloNoticia varchar(45) ,
+in _Descripcion varchar(45) ,
+in _Imagen varchar(45) ,
+in _URL varchar(45) ,
+in _Posicion varchar(45) ,
+in _IdUsuario  int(11) ,
+in _Opcion varchar(45) 
+
+ )
+BEGIN
+ if _Opcion='I' then
+
+insert into Noticias (TituloNoticia,Descripcion,Imagen,URL,Posicion,IdUsuario) values(_TituloNoticia,_Descripcion,_Imagen,_URL,_Posicion,_IdUsuario);
+
+
+   End IF;
+
+    if _Opcion='D' then
+
+Delete from Noticias where IdNoticias =_IdNoticias ;
+
+   
+   End If;
+   END
+   $$
+
+DELIMITER $$
+ CREATE PROCEDURE PA_Get_Noticias(
+
+     in _Opcion varchar(45)
+ )
+BEGIN
+  
+   
+ if _Opcion='T' then
+
+select * from Noticias  order by IdNoticias desc limit 2  ;
+
+   End IF;
+
+   END
+   $$
---
+++ SQL/ProcedimientosAlmacenados/SPUsuario.sql
@@ -0,0 +1,51 @@
+Use pagadmin	;
+
+DELIMITER $$
+ CREATE PROCEDURE PA_Set_Usuario(
+in _IdUsuario int,
+in _Nombres varchar(45),
+     in _Apellidos varchar(45),
+     in _Correo varchar(45),
+     in _Contraseña varchar(45),
+     in _Estado varchar(45),
+in _Opcion varchar(45)
+
+ )
+BEGIN
+ if _Opcion='I' then
+
+insert into Usuario (Nombres, Apellidos,Correo,Contraseña,Estado ) values(_Nombres, _Apellidos,_Correo,_Contraseña,_Estado );
+
+   End IF;
+
+    if _Opcion='D' then
+
+Update Usuario set Estado=_Estado where IdUsuario=_IdUsuario ;
+   
+   End If;
+   END
+   $$
+
+DELIMITER $$
+ CREATE PROCEDURE PA_Get_Usuario(
+in _Correo varchar(45),
+     in _Contraseña varchar(45),
+     in _Opcion varchar(45)
+ )
+BEGIN
+
+ if _Opcion='L' then
+
+select * from usuario where Contraseña=_Contraseña and Correo=_Correo;
+
+   End IF;
+   
+   
+ if _Opcion='T' then
+
+select * from usuario order by idusuario desc;
+
+   End IF;
+
+   END
+   $$
