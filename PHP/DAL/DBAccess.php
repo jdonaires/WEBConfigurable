5ecf146385a8d8625fe2b20c0e988745bbc7f85d
VictorMunayco96 <m.m.victor9630@gmail.com>
2018-11-05 15:22:48 -0500

    CAmbios

--- PHP/DAL/DBAccess.php
+++ PHP/DAL/DBAccess.php
@@ -5,12 +5,12 @@ class DBAccess
   public function __CONSTRUCT()
   {
     try {
-      $this->conn = new PDO('mysql:host=localhost; dbname=pagadmin', 'Visitante', '123456');
+      $this->conn = new PDO('mysql:host=localhost; dbname=pagadmin', 'root', '123456');
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			
+
     } catch (PDOException $e ) {
       echo "error:" .$e->getMessage();
-	 
+
     }
  }
 
