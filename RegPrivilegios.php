626de34770ad37cb39d6a39e4ce71b187b588036
WPerezHernandez <pwendypamela@gmail.com>
2018-11-05 16:44:54 -0500

    Modificaciones
    Redireccionamiento

--- RegPrivilegios.php
+++ RegPrivilegios.php
@@ -2,8 +2,9 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Cabecera</title>
+	<title>Agregar usuario</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 	<style>
 		header{
@@ -41,23 +42,23 @@
 	</style>
 </head>
 <body>
-	<div class="container-fluid">
+<div class="container-fluid">
 		<header class="row">
 			<div class="col-3 col-xs-12">
-				<img  src="img/logo.png" alt="Logo Empresa" class="img-fluid logo">
+				<img  src="icono_config/logo.png" alt="Logo Empresa" class="img-fluid logo">
 			</div>
-			<div class="col-1 offset-5 text-right">
-				<img  src="img/user.jpg" alt="Foto" class="rounded-circle foto" width="65" height="65">
+			<div class="col-1 offset-4">
+				<img  src="icono_config/user.jpg" alt="Foto" class="rounded-circle foto" width="65" height="65">
 			</div>
-			<div class="col-2-auto text-right">
+			<div class="col-4">
 				<p>Usuario Administrador</p>
-				<p>Martinez Perez, Juan</p>
+				<p>Magallanes Magallanes, Fáride</p>
 			</div>
-			<div class="col-1 text-center align-self-center">
-				<button class="btn btn-outline-secondary"><img src="icono_config/icono_salir.png"></button>
+			<div class="col-1 offset-7">
+	 	<button class="btn btn-link" style= "position: absolute;bottom: 10px">
+		<img src="icono_config/icono_salir.png"></button>
 			</div>
 		</header>
-
 		<section class="row menu">
 			<div class="col-12">
 				<div class="btn-group" role="group" aria-label="Basic example">
@@ -72,8 +73,7 @@
 				    <a class="dropdown-item" href="RegPrivilegios.php">Privilegios</a>
 				  </div>
 				</div>
-
-				<div class="dropdown opcion_menu">
+		<div class="dropdown opcion_menu">
 				  <button class="btn btn-link items dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				    Portada Principal
 				  </button>
@@ -107,58 +107,29 @@
 
 				</div>
 			</div>
-		</section>
 
-		<section class="row contenido justify-content-center">
-			<div class="col-md-8">
+			<div class="col-md-8 offset-md-1">
 				<div class="card">
 				  <div class="card-header">
-				    	Registro Cabecera
+				    Usuario de Servicio
 				  </div>
 				  <div class="card-body">
-				    <form>
-					  	<div class="form-group row">
-					    	<label for="nombre" class="col-sm-4 col-form-label">Nombre de la Organizacion:</label>
-					    	<div class="col-sm-8">
-					      		<input type="text" class="form-control" id="nombre" placeholder="Nombre de la Organizacion">
-					    	</div>
-					  	</div>
-					  	<div class="form-group row">
-					    	<label for="email" class="col-sm-4 col-form-label">Email:</label>
-					    	<div class="col-sm-8">
-					      		<input type="email" class="form-control" id="email" placeholder="email@example.com">
-					    	</div>
-					  	</div>
-					  	<div class="form-group row">
-					    	<label for="telefono" class="col-sm-4 col-form-label">Telefono:</label>
-					    	<div class="col-sm-4">
-					      		<input type="text" class="form-control" id="telefono" placeholder="Telefono">
-					    	</div>
-					  	</div>
-					  	<div class="form-group row">
-					  		<div class="col-sm-4">Logotipo:</div>
-					    	<div class="col-sm-8">
-					      		<input type="file" id="archivo">
-					    	</div>
-					  	</div>
-					  	
-					  	<div class="form-group row">
-					  		<div class="col-sm-4"></div>
-					  		<div class="col-sm-3">
-					      		<button class="btn btn-danger text-center" style="background: green; border: green; color:white" >
-					      			<img src="img/icono_actualizar.png">&nbsp Actualizar
-					      		</button>
-					    	</div>
-					  	</div>
-						
-					</form>
-				  </div>
-				</div>
-			</div>
-		</section>
 
-	</div>
+		<form method="post" action="registrar.php">
+
+<fieldset border="2px" style="width: 380px"> 
 	
+	<input type="checkbox" name="opcion" value="Administrador de Slider"> Administrador de Slider <br>
+	<input type="checkbox" name="opcion" value="Administrador de Nosotros"> Administrador de Nosotros <br>
+	<input type="checkbox" name="opcion" value="Administrador de Fondo"> Administrador de Fondo <br> <br>
+
+	<button class="btn btn-green text-center" style="background: green; border: green; color:white">
+	<img src="icono_config/icono_guardar.png">&nbsp Guardar</button>
+	<button class="btn btn-green text-center" style="background: green; border: green; color:white">
+	<img src="icono_config/icono_cerrar.png">&nbsp Cancelar</button>
+
+	</form>
+</fieldset>
 
 	<script src="js/jquery.js"></script>
 	<script src="js/popper.min.js"></script>
