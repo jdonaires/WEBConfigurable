cffe60304818141e70501bde3d3f356cfed846d6
WPerezHernandez <pwendypamela@gmail.com>
2018-11-10 20:14:49 -0500

    Actualizaciones a la pagina
    Actualizaciones a la pagina

--- RegUsuario.php
+++ RegUsuario.php
@@ -74,10 +74,10 @@ header('Location: RegUsuario.php');
 			<div class="col-3 col-xs-12">
 				<img  src="icono_config/logo.png" alt="Logo Empresa" class="img-fluid logo">
 			</div>
-			<div class="col-1 offset-4">
+			<div class="col-1 offset-5 text-right">
 				<img  src="icono_config/user.jpg" alt="Foto" class="rounded-circle foto" width="65" height="65">
 			</div>
-			<div class="col-4">
+			<div class="col-2-auto text-right">
 				<p>Usuario Administrador</p>
 				<p>Magallanes Magallanes, Fáride</p>
 			</div>
@@ -105,20 +105,15 @@ header('Location: RegUsuario.php');
 				    Portada Principal
 				  </button>
 				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-				    <a class="dropdown-item" href="#">Cabecera de Portada</a>
-				    <a class="dropdown-item" href="#">Redes Sociales</a>
-				    <a class="dropdown-item" href="#">Slider</a>
-				    <a class="dropdown-item" href="#">Pie de Portada</a>
-				  </div>
-				</div>
-
-				<div class="dropdown opcion_menu">
-				  <button class="btn btn-link items dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				    Secciones Inicio
-				  </button>
-				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-				    <a class="dropdown-item" href="#">Conoce mas...</a>
-				    <a class="dropdown-item" href="#">Noticias</a>
+				    <a class="dropdown-item" href="RegCabecera.php">Registro Cabecera</a>
+					<a class="dropdown-item" href="Lista_ConoceMas.php">Lista Conoce mas</a>
+				    <a class="dropdown-item" href="Lista_de_Noticias.php">Lista de Noticias</a>
+				    <a class="dropdown-item" href="Lista_Redes_Sociales.php">Lista de Redes Sociales</a>
+				    <a class="dropdown-item" href="ListaSlider.php">Lista Slider</a>
+					<a class="dropdown-item" href="RegSlider.php">Registro Slider</a>
+					<a class="dropdown-item" href="RegNostrosIndex.php">Registro Nosotros</a>
+					<a class="dropdown-item" href="RegRedesSociales.php">Registro Redes Sociales</a>
+					
 				  </div>
 				</div>
 
@@ -127,8 +122,8 @@ header('Location: RegUsuario.php');
 				    Nosotros
 				  </button>
 				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-				    <a class="dropdown-item" href="#">Imagen de Portada</a>
-				    <a class="dropdown-item" href="#">Contenido</a>
+				    <a class="dropdown-item" href="RegNosotrosImagenFondo.php">Imagen de Portada</a>
+				    <a class="dropdown-item" href="RegNoticia.php">Contenido</a>
 				  </div>
 				</div>
 
